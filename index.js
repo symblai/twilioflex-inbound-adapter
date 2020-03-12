f7d1e1003c0a24a4c12143117729bc9625847af2
ks436 <56366500+ks436@users.noreply.github.com>
2020-03-11 19:57:27 -0700

    update for rebranding

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 /**
   * Sample inbound integration showing how to use Twilio Flex
-  * with Rammer's websocket API as the inbound audio stream
+  * with Symbl's websocket API as the inbound audio stream
   */
 
 /* import necessary modules for the web-socket API */
@@ -82,8 +82,8 @@ ws.on('connection', (conn) => {
       "meetingTitle": "Customer Call"
     },
     "speaker": {
-      "userId": "kunal.sinha@rammer.ai",
-      "name": "Kunal"
+      "userId": "jon@symbl.ai",
+      "name": "Jon"
     },
   }));
 });
@@ -92,7 +92,7 @@ ws.on('connection', (conn) => {
 
 const authOptions = {
   method: 'post',
-  url: 'https://api.rammer.ai/oauth2/token:generate',
+  url: 'https://api.symbl.ai/oauth2/token:generate',
   body: {
     type: "application",
     appId: process.env.APP_ID,
@@ -111,11 +111,11 @@ let auth = new Promise(resolve => {
   })
 });
 
-/* Connect to Rammer's Websocket API */
+/* Connect to Symbl's Websocket API */
 
 auth.then(body => {
   wsc.connect(
-    'wss://api.rammer.ai/v1/realtime/insights/1',
+    'wss://api.symbl.ai/v1/realtime/insights/1',
     null,
     null,
     { 'X-API-KEY': JSON.stringify(body.accessToken)}
